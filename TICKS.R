e5fd4cd0269a6018737523a0122cceb7949c767b
Tao Huang <huangt@caryinstitute.org>
2020-07-16 13:49:20 +0800

    Add files via upload

---
+++ TICKS.R
@@ -0,0 +1,8 @@
+library(neonUtilities)
+
+zipsByProduct(dpID="DP1.10093.001"
+              , site= "all"
+              , startdate = NA
+              , enddate = NA,
+  package = "basic", avg = "all", check.size = TRUE, savepath = NA,
+  load = F)
